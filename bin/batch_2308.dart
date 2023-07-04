106e5a1b26479eea14b4fe1e5b4cc2609fa60721
fuadmostfij6 <fuadmostfij4@gmail.com>
2023-07-05 00:51:34 +0600

    lessone 2

---
+++ bin/batch_2308.dart
@@ -0,0 +1,34 @@
+import 'package:batch_2308/batch_2308.dart' as batch_2308;
+
+
+
+
+int a = 500;
+int b = 200;
+String c = "200mostafij true";
+
+//list
+
+List list = ["fwege",0,true,2.3, 2.3, 2.3];
+//key, value
+Map map ={
+
+  "name":"Mostafij", //key, value
+  "Age":"20",
+  "address":"Dhaka",
+
+};
+
+void main() {
+ // a = 100;
+ //  map.addAll({5: 'Jupiter', 6: 'Saturn'});
+ //  map.
+ // print(map);
+
+  print(list.toList());
+}
+
+
+//dataType=  int, float, bool, String, const, final, var
+
+
---
+++ test/batch_2308_test.dart
@@ -0,0 +1,8 @@
+import 'package:batch_2308/batch_2308.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

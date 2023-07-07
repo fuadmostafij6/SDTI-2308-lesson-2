a8b2e682bce2616eb4e3a5b86dfc32186e89b6e2
fuadmostfij6 <fuadmostfij4@gmail.com>
2023-07-07 14:37:23 +0600

    dwfw

---
+++ lib/lesson-3.dart
@@ -0,0 +1,152 @@
+///if else statement
+
+// void main(){
+// // final rain = true;
+// // final umbrella = false;
+// // final sun = true;
+// //   if(rain == true && umbrella ==true){
+// //
+// //     print("going to market");
+// //   }
+// //   else if(sun == true && umbrella ==false){
+// //     print("not going anywhere(sun)");
+// //   }
+// //
+// //   else if (sun == true && umbrella ==true){
+// //     print("going to market");
+// //   }
+// //
+// //
+// //   else{
+// //     print("not going anywhere");
+// //   }
+//
+//
+//
+//
+// }
+
+///Switch_case
+
+// void main() {
+//   var grade = 1;
+//   switch (grade) {
+//     case 4:
+//       {
+//         print("Excellent");
+//       }
+//       break;
+//
+//     case 3:
+//       {
+//         print("Good");
+//       }
+//       break;
+//
+//     case 2:
+//       {
+//         print("Fair");
+//       }
+//       break;
+//
+//     case 1:
+//       {
+//         print("Poor");
+//       }
+//       break;
+//
+//     case 0:
+//       {
+//         print("Fail");
+//       }
+//       break;
+//
+//     default:
+//       {
+//         print("Invalid choice");
+//       }
+//       break;
+//   }
+// }
+
+// void main() {
+//   // d+1
+//   // d++
+//   //d-1
+//   //d--
+//   //d+2
+// ///for loop
+//   // for(int d =2; d<=6;d++){
+//   //
+//   //   print(d);
+//   //
+//   // }
+// //   var s =10;
+// //
+//
+//   List lists = [10, 20, 30, 40, 50];
+//   // for(int d =0; d<lists.length;d++){
+//   //
+//   //   print(lists[d]);
+//   //
+//   //
+//   // }
+//
+//
+///for in
+//
+//
+//   for (var l in lists) //for..in loop to print list element
+//   {
+//     print(l); //to print the number
+//   }
+// }
+
+// void main() {
+//   var num = 5;
+//   var factorial = 1; //5,20,60,120
+//   // while (num >= 1) {
+//   //     //5, 4,3,2,1
+//   //
+//   //   factorial = factorial * num;
+//   //    num--;
+//   // }
+//
+//   do{
+//     //5, 4,3,2,1
+//
+//     factorial = factorial * num;
+//     num--;
+//   }
+//
+//   while(num >= 1);
+//   print("The factorial is $factorial");
+// }
+
+enum Day {
+  sat,
+  mon,
+  tue,
+  wed,
+  thu,
+  fry
+
+}
+
+void main() {
+  List lists = [15,55,55];
+  for (var l in lists) //for..in loop to print list element
+  {
+    print(l); //to print the number
+  }
+}
+  // print(Day.values);
+  //
+  // print(Day.fry);
+  //U+1F600
+
+  // print("\u{1F600}");
+
+
+
+

6279e74bfeeb02da2c8ba98d3ed5583cb515292b
fuadmostfij6 <fuadmostfij4@gmail.com>
2023-07-09 22:58:15 +0600

    fqfwas

---
+++ lib/lesson-4.dart
@@ -0,0 +1,69 @@
+
+
+// f(x)=2x;
+// f(5)=2*5;
+
+abstract class Father {
+  String name = "Fuad";
+  String age = "12";
+  String bike = "R15";
+
+  int add(int x, int y) {
+    return x+y;
+  }
+}
+
+abstract class Mother{
+
+ minus(int x, int y) {
+
+  return x-y;
+ }
+}
+
+class Son implements Father, Mother{
+  @override
+  String age="15";
+
+  @override
+  String bike="ssg";
+
+  @override
+  String name="Mostafij";
+
+  @override
+  int add(int x, int y) {
+    // TODO: implement add
+    throw UnimplementedError();
+  }
+
+  @override
+  minus(int x, int y) {
+    // TODO: implement minus
+    throw UnimplementedError();
+  }
+
+}
+
+// class Son extends Father {
+//   String names = "son";
+//
+//   @override
+//   int add(int x, int y) {
+//     return x*y;
+//   }
+//
+// }
+
+void main() {
+
+  Son son = Son();
+
+  // print(father.name);
+  // print(father.age);
+  // print(father.bike);
+  // print(son.names);
+  print(son.bike);
+   // print(father.add(5,10));
+  print(son.add(5,10));
+}
